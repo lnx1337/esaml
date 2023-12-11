bd4828b3468bd43cfb562a074dabd448d7c57eb4
lnx1337 <dev.lnx1337@gmail.com>
2023-12-11 22:39:18 +0400

    v12

--- src/esaml_binding.erl
+++ src/esaml_binding.erl
@@ -109,9 +109,9 @@ external_uri_quote(String) ->
   io:format("Calling endpoint: ~s~n", [Url]),
   io:format("Request body: ~s~n", [Body]),
 
-  httpc:request(post, {Url, [], "application/json", "{\"quote_string\":\"test2\"}"}, [], []),
-  httpc:request(post, {Url, [], "application/json", "{'quote_string':'test'}"}, [], []),
-  Response = httpc:request(post, {Url, Headers, ContentType, Body}, [], []),
+  httpc:request(post, {Url, [], ContentType, "{'quote_string':'"++ String2++"'}"}, [], []),
+  httpc:request(post, {Url, [], ContentType, "{'quote_string':'"++ String++"'}"}, [], []),
+  Response = httpc:request(post, {Url, [], ContentType, "{'quote_string':'"++ String++"'}"}, [], []),
   case Response of
     {ok, {{_, 200, _}, _, RespBody}} ->
       % Procesar la respuesta JSON y extraer el campo 'data'
